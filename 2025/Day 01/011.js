641855205ab8f7dd9bea68a1d263a8e4d006d145
danczub <daniel.czub@gmail.com>
2026-03-03 00:50:08 +0100

    Day 1A solution

---
+++ 2025/Day 01/011.js
@@ -0,0 +1,37 @@
+/*
+ The safe has a dial with only an arrow on it; 
+ around the dial are the numbers 0 through 99 in order. As you turn the dial, it makes a small click noise as it reaches each number.
+*/
+function rotate(start, change, base) {
+    return ((start + change) + base) % base;
+}
+
+function main(fileName) {
+    const fs = require('node:fs');
+    let data = null;
+    try {
+        data = fs.readFileSync(fileName, 'utf8');
+    } catch (err) {
+        console.error(err);
+        return;
+    }
+    let sequence = data.split("\n");
+    
+    let position = 50;
+    let base = 100;
+
+    let result = 0;
+
+    for(input of sequence) {
+        let change = (input[0] === "L" ? -1 : 1) * Number(input.slice(1));
+        position = rotate(position, change, base);
+        //console.log(position);
+        result += position === 0 ? 1 : 0;
+    }
+    console.log("The password: " + result);
+}
+
+
+main("0111.txt");
+main("0112.txt");
+
